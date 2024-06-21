ab4c41a3a03260bfa7b4f59b5a8e8baa50a916b1
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-06-21 20:01:55 +0700

    update view home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,12 @@
-<a href="../admin/index.php">halaman admin</a>
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        <a class="navbar-brand">Navbar</a>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<div class="container">
+    <a href="../admin/index.php">halaman admin</a>
+</div>
