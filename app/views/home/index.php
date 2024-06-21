614ede838cb09c2f3e5e41d8f4a6a96f57a7e220
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-06-21 20:42:21 +0700

    update tampilan home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,12 +1,22 @@
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand">Navbar</a>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <a class="navbar-brand" href="#">SILASTRI</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href=".">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pengumuman</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../login/index.php">Login</a>
+            </li>
+        </ul>
     </div>
 </nav>
 <div class="container">
-    <a href="../admin/index.php">halaman admin</a>
+    <h2 class="mt-2 text-center">SELAMAT DATANG DI WEBSITE SILASTRI</h2>
 </div>
