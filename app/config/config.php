ae136d13a69239614ccad752997796fd2271d7a6
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-06-27 23:47:17 +0700

    merapikan kode pada config

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define ('MAIN_URL', 'http://localhost/app_web/public/');
+define('MAIN_URL', 'http://localhost/app_web/public/');
 
 // database
-define ('DB_HOST', 'localhost');
-define ('DB_USER', 'root');
-define ('DB_PASS', '');
-define ('DB_NAME', ''); // isi nama databasenya
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', ''); // isi nama databasenya
