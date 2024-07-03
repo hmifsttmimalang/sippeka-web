315a53425b03510464c1cb523f173ce4cd5f8a9c
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-03 14:40:31 +0700

    penambahan komentar pada config

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,7 @@
 <?php
 
-define('MAIN_URL', 'http://localhost/app_web/public/');
+define('MAIN_URL', 'http://localhost:8080/app_web/public/');
+// define('MAIN_URL', 'http://localhost/app_web/public/'); // jika port di Apachenya adalah 80
 
 // database
 // define('DB_HOST', 'localhost');
