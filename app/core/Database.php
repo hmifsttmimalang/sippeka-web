751f682e651837750283d4df988d4e8553266579
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-03 14:58:29 +0700

    perubahan kode

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Core;
+use PDO;
+use PDOException;
 
 class Database
 {
@@ -38,12 +40,12 @@ class Database
         $dsn = 'sqlite:' . $this->db_name;
 
         $option = [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
 
         try {
-            $this->dbh = new \PDO($dsn, $this->user, $this->pass, $option);
-        } catch (\PDOException $err) {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+        } catch (PDOException $err) {
             die($err->getMessage());
         }
     }
@@ -66,16 +68,16 @@ class Database
         if (is_null($type)) {
             switch (true) {
                 case is_int($value):
-                    $type = \PDO::PARAM_INT;
+                    $type = PDO::PARAM_INT;
                     break;
                 case is_bool($value):
-                    $type = \PDO::PARAM_BOOL;
+                    $type = PDO::PARAM_BOOL;
                     break;
                 case is_null($value):
-                    $type = \PDO::PARAM_NULL;
+                    $type = PDO::PARAM_NULL;
                     break;
                 default:
-                    $type = \PDO::PARAM_STR;
+                    $type = PDO::PARAM_STR;
                     break;
             }
         }
@@ -91,12 +93,12 @@ class Database
     public function resultSet()
     {
         $this->execute();
-        return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function single()
     {
         $this->execute();
-        return $this->stmt->fetch(\PDO::FETCH_ASSOC);
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
