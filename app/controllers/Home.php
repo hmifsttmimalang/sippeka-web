1dc02e02c02034b794e1150443f53f75169cd57d
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-16 02:05:49 +0700

    perbaikan tombol logout dan nama user

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends Controller
 {
     public function index()
     {
+        session_start();
         $data['title'] = 'Halaman Utama';
         $this->view('layout/header', $data);
         $this->view('home/index');
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,30 +1,3 @@
-<body class="index-page">
-  <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center">
-
-      <a href="." class="logo d-flex align-items-center me-auto">
-        <img src="<?= MAIN_URL ?>assets/user-layout/img/logo.png" alt="">
-        <h1 class="sitename">SIPPEKA</h1>
-      </a>
-
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="./" class="active">Beranda</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#services">Services</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
-      <?php if (isset( $_SESSION['username'])) : ?>
-        <a href="#"><?= htmlspecialchars( $_SESSION['username']) ?></a>
-        <a class="btn-primary" href="<?= MAIN_URL ?>auth/logout">Keluar</a>
-      <?php else: ?>
-        <a class="btn-getstarted" href="<?= MAIN_URL ?>auth/login">Masuk</a>
-        <a class="btn-getstarted" href="<?= MAIN_URL ?>auth/register">Daftar</a>
-      <?php endif; ?>
-    </div>
-  </header>
 
   <main class="main">
 
@@ -44,7 +17,6 @@
           <img src="<?= MAIN_URL ?>assets/user-layout/img/hero-services-img.webp" class="img-fluid hero-img" alt="" data-aos="zoom-out" data-aos-delay="300">
         </div>
       </div>
-
     </section>
     <!-- /Hero Section -->
 
@@ -376,66 +348,4 @@
     <!-- /Services Section -->
   </main>
 
-  <footer id="footer" class="footer position-relative light-background">
-    <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">SIPPEKA</span>
-          </a>
-          <div class="footer-contact pt-3">
-            <p>JL. Candi Panggung</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-4 col-md-12 footer-newsletter">
-          <h4>Our Newsletter</h4>
-          <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-          <form action="forms/newsletter.php" method="post" class="php-email-form">
-            <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-          </form>
-        </div>
-      </div>
-    </div>
-    <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">QuickStart</strong><span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer>
+  
