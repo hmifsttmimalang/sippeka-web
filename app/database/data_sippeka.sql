a2ebd03e58eac80e213aecf8bf4c0b7237f81f07
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-20 02:28:52 +0700

    perubahan isi database .sql

--- app/database/data_sippeka.sql
+++ app/database/data_sippeka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 09:59 AM
+-- Generation Time: Jul 19, 2024 at 09:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,8 +45,7 @@ CREATE TABLE `registrations` (
 --
 
 INSERT INTO `registrations` (`id`, `user_id`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_telepon`, `keterangan`) VALUES
-(1, 10, 'Farhan', 'Moskow', '1991-12-26', 'Laki-laki', 'Islam', 'St. Moskva', '234567890', 'Diterima'),
-(2, 11, 'Sigit', 'Padang', '2024-07-01', 'Laki-laki', 'Islam', 'Kota Padang', '1234578905', 'Tidak Diterima');
+(1, 10, 'Farhan', 'Moskow', '1991-12-26', 'Laki-laki', 'Islam', 'St. Moskva', '234567890', 'Sedang Diproses');
 
 -- --------------------------------------------------------
 
@@ -60,17 +59,17 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','user') NOT NULL DEFAULT 'user'
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `registered` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `role`) VALUES
-(9, 'Admin', 'admin', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 'admin'),
-(10, 'Farhan Kebab', 'farhankebab', 'farhan@sigma.com', '1ac5012170b65fb99f171ad799d045ac', 'user'),
-(11, 'Sigit', 'sigitrendang', 'sigit@sigma.com', 'd6916d248b949bb73ee7066f567151f2', 'user');
+INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `role`, `registered`) VALUES
+(9, 'Admin', 'admin', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 'admin', 0),
+(10, 'Farhan Kebab', 'farhankebab', 'farhan@sigma.com', '1ac5012170b65fb99f171ad799d045ac', 'user', 1);
 
 --
 -- Indexes for dumped tables
@@ -105,7 +104,7 @@ ALTER TABLE `registrations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
