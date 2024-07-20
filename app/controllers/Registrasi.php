886d91ece8a2fdc80ee4ebb2112296b4d5336617
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-20 19:58:59 +0700

    perbaikan pada akses registrasi

--- app/controllers/Registrasi.php
+++ app/controllers/Registrasi.php
@@ -16,7 +16,7 @@ class Registrasi extends Controller
     public function index()
     {
         session_start();
-        if (!isset($_SESSION['user_id']) && $_SESSION['role'] !== 'user') {
+        if (!isset($_SESSION['role']) && $_SESSION['role'] !== 'user') {
             header('Location: ' . MAIN_URL . 'auth/login');
             exit;
         }
