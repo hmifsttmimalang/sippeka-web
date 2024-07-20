ed439ab4ea401c3f5b37576069f2105bfc38e1bf
ardie069 <93079627+ardie069@users.noreply.github.com>
2024-07-20 20:30:32 +0700

    membatasi akses apabila login bukan sebagai user

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,9 +4,21 @@ use App\Core\Controller;
 
 class Home extends Controller 
 {
-    public function index()
+
+    public function __construct()
     {
         session_start();
+
+        if (isset($_SESSION['user_id']) && $_SESSION['user_role'] === 'user') {
+            return;
+        } else {
+            header('Location: '. MAIN_URL . 'auth/login');
+            exit;
+        }
+    }
+
+    public function index()
+    {
         $data['title'] = 'Halaman Utama';
         $this->view('layout/header', $data);
         $this->view('home/index');
