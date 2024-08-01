f2b611fb52d343e860c3cde453248bcd26aa898a
ardie069 <ardi.pulaem069@gmail.com>
2024-08-02 00:28:04 +0700

    perubahan pada tampilan admin

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -12,6 +12,61 @@ class AdminController
 
     public function index()
     {
-        include 'views/admin/dashboard.php';
+        include 'views/admin/dashboard_admin.php';
+    }
+
+    public function kelolaData()
+    {
+        include 'views/admin/kelola_data.php';
+    }
+
+    public function peserta()
+    {
+        include 'views/admin/peserta.php';
+    }
+
+    public function infoUser()
+    {
+        include 'views/admin/info_user.php';
+    }
+
+    // halaman soal keahlian
+
+    public function soalKeahlian()
+    {
+
+    }
+
+    public function TambahSoalKeahlian()
+    {
+
+    }
+
+    public function UbahSoalKeahlian()
+    {
+
+    }
+
+    public function HapusSoalKeahlian()
+    {
+
+    }
+
+    // kelas keahlian
+    public function kelasKeahlian()
+    {
+
+    }
+
+    // tes keahlian
+    public function tesKeahlian()
+    {
+
+    }
+
+    // sesi keahlian
+    public function sesiKeahlian()
+    {
+
     }
 }
