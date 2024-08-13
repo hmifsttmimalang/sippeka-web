6eb459ab0c3af7fa354415bfd3ae7fb7fea284c6
ardie069 <ardi.pulaem069@gmail.com>
2024-08-13 18:36:20 +0700

    perubahan tampilan pendaftaran di konten

--- views/pendaftaran/form_registrasi.php
+++ views/pendaftaran/form_registrasi.php
@@ -1,31 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SIPPEKA | Pendaftaran</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
-
-    <style>
-        body.bg_register {
-            background-color: #f6f9ff;
-        }
-    </style>
-
-</head>
-
 <body class="bg_register">
 
     <div class="container">
@@ -102,9 +74,9 @@
                                             <select name="keahlian" id="keahlian" class="form-control">
                                                 <option value="Pilih Keahlian">Pilih Keahlian</option>
                                                 <?php if (!empty($keahlianList)) : ?>
-                                                <?php foreach ($keahlianList as $item) : ?>
-                                                    <option value="<?= $item['nama'] ?>"><?= $item['nama'] ?></option>
-                                                <?php endforeach; ?>
+                                                    <?php foreach ($keahlianList as $item) : ?>
+                                                        <option value="<?= $item['nama'] ?>"><?= $item['nama'] ?></option>
+                                                    <?php endforeach; ?>
                                                 <?php endif; ?>
                                             </select>
                                         </div>
@@ -144,17 +116,3 @@
         </div>
 
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="assets/admin/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="assets/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="assets/admin/js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
