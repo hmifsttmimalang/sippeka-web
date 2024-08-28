197ebcea20cb8235acedd1efddfd34db7b58102b
ardie069 <ardi.pulaem069@gmail.com>
2024-08-28 20:41:28 +0700

    perubahan field pada migrasi tabel registrasi

--- sippeka/database/migrations/2024_08_28_120530_create_registrations_table.php
+++ sippeka/database/migrations/2024_08_28_120530_create_registrations_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('tempat_lahir');
             $table->date('tanggal_lahir');
-            $table->string('jenis_kelamin');
+            $table->enum('jenis_kelamin', ['L', 'P']);
             $table->string('agama');
             $table->string('alamat');
             $table->string('telepon');
