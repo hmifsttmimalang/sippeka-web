4913e5ee222ae5cdb08b4153d14d14436b2c78e1
ardie069 <ardi.pulaem069@gmail.com>
2024-08-28 20:14:10 +0700

    menghapus migrasi yang tidak diperlukan

--- sippeka/database/migrations/2024_08_28_120530_create_registrations_table.php
+++ sippeka/database/migrations/2024_08_28_120530_create_registrations_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('telepon');
+            $table->string('keahlian');
+            $table->string('foto_ktp')->nullable();
+            $table->string('foto_ijazah')->nullable();
+            $table->string('foto_bg_biru')->nullable();
+            $table->string('foto_kk')->nullable();
             $table->timestamps();
         });
     }
