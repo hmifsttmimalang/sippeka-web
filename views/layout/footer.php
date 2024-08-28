4694cfe5be4b20cce7de6b49f153aa891f8a9cf9
ardie069 <ardi.pulaem069@gmail.com>
2024-08-28 18:16:22 +0700

    penambahan tampilan utama pada laravel

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,56 +1,55 @@
-
 <footer id="footer" class="footer position-relative light-background">
 
-<div class="container footer-top">
-  <div class="row">
-    <div class="col-lg-6 col-md-6 footer-about">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <span class="sitename">SIPPEKA</span>
-      </a>
-      <div class="footer-contact pt-3">
-        <p>Jl. Raya Singosari, Song Song, Ardimulyo, </p>
-        <p>Kec. Singosari, Kabupaten Malang, Jawa Timur, 65153</p>
-        <p class="mt-3"><strong>Telepon Kantor :</strong> <span>0341 - 458055</span></p>
-        <p class="mt-3"><strong>WhatsApp:</strong> <span>08233435222</span></p>
-        <p><strong>Email:</strong> <span>blk.singosari.jatim@gmail.com</span></p>
+  <div class="container footer-top">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 footer-about">
+        <a href="index.html" class="logo d-flex align-items-center">
+          <span class="sitename">SIPPEKA</span>
+        </a>
+        <div class="footer-contact pt-3">
+          <p>Jl. Raya Singosari, Song Song, Ardimulyo, </p>
+          <p>Kec. Singosari, Kabupaten Malang, Jawa Timur, 65153</p>
+          <p class="mt-3"><strong>Telepon Kantor :</strong> <span>0341 - 458055</span></p>
+          <p class="mt-3"><strong>WhatsApp:</strong> <span>08233435222</span></p>
+          <p><strong>Email:</strong> <span>blk.singosari.jatim@gmail.com</span></p>
+        </div>
+
+        <div class="social-links d-flex mt-4">
+          <a href=""><i class="bi bi-twitter-x"></i></a>
+          <a href=""><i class="bi bi-facebook"></i></a>
+          <a href=""><i class="bi bi-instagram"></i></a>
+          <a href=""><i class="bi bi-linkedin"></i></a>
+        </div>
       </div>
 
-      <div class="social-links d-flex mt-4">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
-    </div>  
 
-    
-    <div class="col-lg-4 col-md-6 footer-newsletter">
-    <h4>Our Newsletter</h4>
-    <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-    <form action="forms/newsletter.php" method="post" class="php-email-form">
-        <div class="newsletter-form">
+      <div class="col-lg-4 col-md-6 footer-newsletter">
+        <h4>Our Newsletter</h4>
+        <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
+        <form action="forms/newsletter.php" method="post" class="php-email-form">
+          <div class="newsletter-form">
             <input type="email" name="email" placeholder="Email Kamu" class="form-control me-2">
             <input type="submit" value="Subscribe">
             <div class="loading">Loading</div>
             <div class="error-message"></div>
             <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-        </div>
-    </form>
-</div>
+          </div>
+        </form>
+      </div>
 
+    </div>
   </div>
-</div>
 
-<div class="container copyright text-center mt-4">
-  <p>© <span>Copyright</span> <strong class="px-1 sitename">SIPPEKA</strong><span>All Rights Reserved</span></p>
-  <div class="credits">
-    <!-- All the links in the footer should remain intact. -->
-    <!-- You can delete the links only if you've purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-    Designed by <a href="https://bootstrapmade.com/">SIPPEKA</a>
+  <div class="container copyright text-center mt-4">
+    <p>© <span>Copyright</span> <strong class="px-1 sitename">SIPPEKA</strong><span>All Rights Reserved</span></p>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you've purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+      Designed by <a href="https://bootstrapmade.com/">SIPPEKA</a>
+    </div>
   </div>
-</div>
 
 </footer>
 
