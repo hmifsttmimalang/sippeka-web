81177b656236c9df5b651e92e409127a2ce965c3
ardie069 <ardi.pulaem069@gmail.com>
2024-09-09 22:47:47 +0700

    menghapus library js yang tidak digunakan

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -35,7 +35,6 @@
     <!-- Vendor JS Files -->
     <script src="{{ asset('assets/profile/vendor/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('assets/profile/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/profile/vendor/chart.js/chart.min.js') }}"></script>
     <script src="{{ asset('assets/profile/vendor/quill/quill.min.js') }}"></script>
     <script src="{{ asset('assets/profile/vendor/simple-datatables/simple-datatables.js') }}"></script>
     <script src="{{ asset('assets/profile/vendor/php-email-form/validate.js') }}"></script>
