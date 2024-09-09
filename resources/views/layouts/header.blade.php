411c1a2a8ff48f12df45ce25b58c77d8b8e11f22
ardie069 <ardi.pulaem069@gmail.com>
2024-09-09 16:20:12 +0700

    menambah header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+        <a href="/" class="logo d-flex align-items-center me-auto">
+            <img src="{{ asset('assets/user/img/silastri/logo_jatim.png') }}" alt="Logo Jatim">
+            <h1 class="sitename">SIPPEKA</h1>
+        </a>
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="active">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#services">Services</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+        <a class="btn-getstarted" href="/login">Masuk</a>
+        <a class="btn-getstarted" href="/register">Buat Akun</a>
+    </div>
+</header>
