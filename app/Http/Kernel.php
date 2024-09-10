57072b1d783b81ddb40b82e7e3bff6c98cd7117f
ardie069 <ardi.pulaem069@gmail.com>
2024-09-10 14:32:24 +0700

    penambahan middleware bagi user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends HttpKernel
 
     protected $routeMiddleware = [
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'user' => \App\Http\Middleware\UserMiddleware::class,
     ];
 
     /**
