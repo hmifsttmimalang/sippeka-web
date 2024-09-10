07012f663c41952c84eccd558567ad56287e7636
ardie069 <ardi.pulaem069@gmail.com>
2024-09-10 11:18:11 +0700

    perubahan pada model dan migrasi pada registrasi

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -4,8 +4,67 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Skill;
+use Exception;
 
 class Registration extends Model
 {
     use HasFactory;
+
+    protected $table = 'registrations';
+    protected $fillable = [
+        'user_id',
+        'nama',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'agama',
+        'alamat',
+        'telepon',
+        'keahlian',
+        'foto_ktp',
+        'foto_ijazah',
+        'foto_bg_biru',
+        'foto_kk',
+        'nilai_keahlian',
+        'nilai_wawancara'
+    ];
+
+    public function keahlian() 
+    {
+        return $this->belongsTo(Skill::class, 'keahlian');
+    }
+
+    public function validateTanggalLahir($tanggal_lahir)
+    {
+        $dateOfBirth = new \DateTime($tanggal_lahir);
+        $today = new \DateTime();
+        $age = $today->diff($dateOfBirth)->y;
+
+        if ($age < 17 || $age >= 40) {
+            throw new Exception('Anda harus berusia minimal 17 tahun dan maksimal 40 tahun untuk mendaftar!');
+        }
+
+        return true;
+    }
+
+    public function saveTesKeahlian($user_id, $nilai_keahlian)
+    {
+        $this->where('user_id', $user_id)->update(['nilai_keahlian' => $nilai_keahlian]);
+    }
+
+    public function getNilaiTesKeahlian($user_id)
+    {
+        return $this->where('user_id', $user_id)->value('nilai_keahlian');
+    }
+
+    public function saveTesWawancara($user_id, $nilai_wawancara)
+    {
+        $this->where('user_id', $user_id)->update(['nilai_wawancara' => $nilai_wawancara]);
+    }
+
+    public function deleteRegistration($id)
+    {
+        $this->find($id)->delete();
+    }
 }
--- database/migrations/2024_09_10_035456_create_registrations_table.php
+++ database/migrations/2024_09_10_035456_create_registrations_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->text('alamat');
+            $table->string('telepon');
+            $table->unsignedBigInteger('keahlian');
+            $table->string('foto_ktp');
+            $table->string('foto_ijazah');
+            $table->string('foto_bg_biru');
+            $table->string('foto_kk');
+            $table->integer('nilai_keahlian')->nullable();
+            $table->integer('nilai_wawancara')->nullable();
             $table->timestamps();
         });
     }
