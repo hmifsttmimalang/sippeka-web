52ee1576d98f8974435e5133688f003aa8b158b6
ardie069 <ardi.pulaem069@gmail.com>
2024-09-10 11:19:01 +0700

    penambahan pada field migrasi keahlian dan mata soal

--- database/migrations/2024_09_10_035540_create_question_titles_table.php
+++ database/migrations/2024_09_10_035540_create_question_titles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('question_titles', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
             $table->timestamps();
         });
     }
