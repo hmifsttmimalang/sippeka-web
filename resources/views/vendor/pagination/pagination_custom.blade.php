80121fe2c7e452e0df180ce2b217a39f3e20b1f1
ardie069 <ardi.pulaem069@gmail.com>
2024-09-19 16:12:19 +0700

    pembaruan pagination

--- resources/views/vendor/pagination/pagination_custom.blade.php
+++ resources/views/vendor/pagination/pagination_custom.blade.php
@@ -10,26 +10,69 @@
             </li>
         @endif
 
-        @foreach ($elements as $element)
-            @if (is_string($element))
-                <li class="page-item disabled">
-                    <span class="page-link">{{ $element }}</span>
-                </li>
-            @elseif (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active" aria-current="page">
-                            <span class="page-link">{{ $page }}</span>
-                        </li>
-                    @else
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                        </li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
+        @php
+            $totalPages = $paginator->lastPage();
+        @endphp
 
+        {{-- Jika total halaman kurang dari 10, tampilkan normal --}}
+        @if ($totalPages <= 10)
+            @for ($i = 1; $i <= $totalPages; $i++)
+                @if ($i == $paginator->currentPage())
+                    <li class="page-item active" aria-current="page">
+                        <span class="page-link">{{ $i }}</span>
+                    </li>
+                @else
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                    </li>
+                @endif
+            @endfor
+        @else
+            {{-- Jika lebih dari 10 halaman, tampilkan dengan elipsis --}}
+            @php
+                $start = max(1, $paginator->currentPage() - 2); // Halaman awal dari kelompok
+                $end = min($totalPages, $paginator->currentPage() + 2); // Halaman akhir dari kelompok
+            @endphp
+
+            {{-- Tombol halaman pertama jika terlalu jauh dari kelompok --}}
+            @if ($start > 1)
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url(1) }}">1</a>
+                </li>
+                @if ($start > 2)
+                    <li class="page-item disabled">
+                        <span class="page-link">...</span>
+                    </li>
+                @endif
+            @endif
+
+            {{-- Loop halaman di sekitar halaman saat ini --}}
+            @for ($i = $start; $i <= $end; $i++)
+                @if ($i == $paginator->currentPage())
+                    <li class="page-item active" aria-current="page">
+                        <span class="page-link">{{ $i }}</span>
+                    </li>
+                @else
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                    </li>
+                @endif
+            @endfor
+
+            {{-- Tombol halaman terakhir jika terlalu jauh dari kelompok --}}
+            @if ($end < $totalPages)
+                @if ($end < $totalPages - 1)
+                    <li class="page-item disabled">
+                        <span class="page-link">...</span>
+                    </li>
+                @endif
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url($totalPages) }}">{{ $totalPages }}</a>
+                </li>
+            @endif
+        @endif
+
+        {{-- Tombol Next --}}
         @if ($paginator->hasMorePages())
             <li class="page-item">
                 <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Next</a>
