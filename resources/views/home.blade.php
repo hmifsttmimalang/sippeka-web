64ea4a8864f21a0c259cbc30717b4a646817ff74
ardie069 <ardi.pulaem069@gmail.com>
2024-09-25 08:27:59 +0700

    perubahan tulisan pada pengumuman

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -285,7 +285,7 @@
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
                 <h2>Pengumuman</h2>
-                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+                <p>Informasi penting</p>
             </div>
             <!-- End Section Title -->
 
