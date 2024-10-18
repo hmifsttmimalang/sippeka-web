e95d5011e2696eeec6a8f1cba9508955f82d954a
ardie069 <ardi.pulaem069@gmail.com>
2024-10-18 08:19:44 +0700

    perubahan testing migrasi database

--- database/seeders/RegistrationSeeder.php
+++ database/seeders/RegistrationSeeder.php
@@ -14,6 +14,6 @@ class RegistrationSeeder extends Seeder
     public function run(): void
     {
         // Membuat beberapa data pendaftar menggunakan factory
-        Registration::factory(50)->create();
+        Registration::factory(100)->create();
     }
 }
