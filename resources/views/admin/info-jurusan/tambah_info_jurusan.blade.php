fc0868e493a104f909163011836dc95836a1aaf8
ardie069 <ardi.pulaem069@gmail.com>
2024-10-23 16:59:43 +0700

    perubahan toggle route

--- resources/views/admin/info-jurusan/tambah_info_jurusan.blade.php
+++ resources/views/admin/info-jurusan/tambah_info_jurusan.blade.php
@@ -80,8 +80,8 @@
                 </a>
                 <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item loadPage" href="{{ route('admin.info_jurusan') }}">Jurusan</a>
-                        <a class="collapse-item loadPage active" href="{{ route('admin.jadwal_tes') }}">Jadwal Tes</a>
+                        <a class="collapse-item loadPage active" href="{{ route('admin.info_jurusan') }}">Jurusan</a>
+                        <a class="collapse-item loadPage" href="{{ route('admin.jadwal_tes') }}">Jadwal Tes</a>
                     </div>
                 </div>
             </li>
